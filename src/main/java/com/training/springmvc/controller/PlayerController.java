8c3fb54892d06e165379bf8248fdb69b94f70951
Adrian Bala <aadrianbala@gmail.com>
2018-10-29 14:05:38 +0100

    Add PlayerController class in controller package

---
+++ src/main/java/com/training/springmvc/controller/PlayerController.java
@@ -0,0 +1,25 @@
+package com.training.springmvc.controller;
+
+import com.training.springmvc.dao.PlayerDAO;
+import com.training.springmvc.entity.Player;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/player")
+public class PlayerController {
+
+    @Autowired
+    private PlayerDAO playerDAO;
+
+    @RequestMapping("/list")
+    public String listPlayers(Model theModel) {
+        List<Player> players = playerDAO.getPlayers();
+        theModel.addAttribute("players", players);
+        return "list-players";
+    }
+}

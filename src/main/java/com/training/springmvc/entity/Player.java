0472644039e1bc4116bcb992afe7547cca592a0a
Adrian Bala <aadrianbala@gmail.com>
2018-11-29 10:11:57 +0100

    Add field notices with getters and setters to Player class in entity package

--- src/main/java/com/training/springmvc/entity/Player.java
+++ src/main/java/com/training/springmvc/entity/Player.java
@@ -1,8 +1,8 @@
 package com.training.springmvc.entity;
 
-import net.bytebuddy.dynamic.loading.InjectionClassLoader;
-
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name="player")
@@ -20,6 +20,9 @@ public class Player {
 
     @Column
     private String discipline;
+
+    @OneToMany(mappedBy = "player", cascade = CascadeType.ALL)
+    private List<Notice> notices;
 
     public Player() {
     }
@@ -54,6 +57,22 @@ public class Player {
 
     public void setDiscipline(String discipline) {
         this.discipline = discipline;
+    }
+
+    public List<Notice> getNotices() {
+        return notices;
+    }
+
+    public void setNotices(List<Notice> notices) {
+        this.notices = notices;
+    }
+
+    public void addNotice(Notice notice) {
+        if(notices == null) {
+            notices = new ArrayList<>();
+        }
+        notices.add(notice);
+        notice.setPlayer(this);
     }
 
     @Override
